03b780fb343608e0f3ec3e0faa0a3a58cc33a23b
Christopher Isene <christopher.isene@gmail.com>
2023-10-18 01:34:37 +0200

    Tweak

--- scripts/bash/mirror.sh
+++ scripts/bash/mirror.sh
@@ -55,10 +55,10 @@ function fixCharacterAmpersand {
   echo "fixCharacterAmpersand - start"
 
   # AMP - Escape ampersant in links
-  sed -i "s|\b\x26\b|&amp;|gi" "$1"
+  sed -i "s#\b\x26\b#&amp;#gi" "$1"
 
   # AMP - Escape stuff that hasn't been escaped already
-  sed -i "s|\x26(?!(?:apos|quot|[gl]t|amp|deg)\x3b|#)|&amp;|gi" "$1"
+  sed -i "s#\x26(?!(?:apos|quot|[gl]t|amp|deg)\x3b|#)#&amp;#gi" "$1"
 
   echo "fixCharacterAmpersand - end"
 }
@@ -306,28 +306,6 @@ MirrorOPML "https://fyyd.de/user/dirkprimbs/collection/fotografiepodcasts/opml" 
 MirrorOPML "https://fyyd.de/user/dirkprimbs/collection/podcastpodcasts/opml" "fyyd-de-dirkprimbs-collection-podcastpodcasts.opml"
 MirrorOPML "https://fyyd.de/user/emolotow/collection/f361eea6f2288b3b565324885c91290a/opml" "fyyd-de-emoltow-collection.opml"
 MirrorOPML "https://fyyd.de/user/garneleh/collection/audiospass-fuer-kids-und-co/opml" "fyyd-de-garneleh-collection-audiospass-fuer-kids-und-co.opml"
-#MirrorOPML "https://fyyd.de/user/garneleh/collection/frauenstimmen-im-netz/opml" "fyyd-de-garneleh-collection-frauenstimmen-im-netz.opml"
-#MirrorOPML "https://fyyd.de/user/gglnx/collection/meine-podcasts/opml" "fyyd-de-gglnx-collection-meine-podcasts.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/geschichte/opml" "fyyd-de-hoersuppe-collection-geschinchte.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/hoersuppe/opml" "fyyd-de-hoersuppe-collection-hoersuppe.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/holgis-podcasts/opml" "fyyd-de-hoersuppe-collection-holgis-podcasts.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/podcasting/opml" "fyyd-de-hoersuppe-collection-podcasting.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/podcasts-fuer-kinder/opml" "fyyd-de-hoersuppe-collection-podasts-fuer-kinder.opml"
-#MirrorOPML "https://fyyd.de/user/hoersuppe/collection/utopia/opml" "fyyd-de-hoersuppe-collection-utopia.opml"
-#MirrorOPML "https://fyyd.de/user/holgi/collection/holgi/opml" "fyyd-de-holgi-collection-holgi.opml"
-#MirrorOPML "https://fyyd.de/user/iwmm/collection/77c5dd6f7db4444989918a5726b90255/opml" "fyyd-de-iwmm-collection.opml"
-#MirrorOPML "https://fyyd.de/user/JaegersNet/collection/de-rpg-podcasts/opml" "fyyd-de-jaegersnet-collection-de-rpg-podcasts.opml"
-#MirrorOPML "https://fyyd.de/user/limpc0re/collection/a64e2c64ea98e27fa996effb1d44af0f/opml" "fyyd-de-limpc0re-collection.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/32dc7ac001dadd43706168d80024d7bb/opml" "fyyd-de-misterl-collection-filme-serien.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/6bb99465e345d025c4ffe8b8bd5f8ff6/opml" "fyyd-de-misterl-collection-albernes.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/8303a30eaffb78e30781753fa08fc769/opml" "fyyd-de-misterl-collection-it.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/9e39ab6ee95f5f4ba8b0c3368cd1828c/opml" "fyyd-de-misterl-collection-politik.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/c50796e5a93ed2db202a6315780dc548/opml" "fyyd-de-misterl-collection-jura.opml"
-#MirrorOPML "https://fyyd.de/user/MisterL/collection/cae73c22ce49638a4b57b5a6eac2420d/opml" "fyyd-de-misterl-collection-belletristik.opml"
-#MirrorOPML "https://fyyd.de/user/ophmoph/collection/36c66142c31e4cd8edbe74c6b82b5483/opml" "fyyd-de-ophmoph-collection.opml"
-#MirrorOPML "https://fyyd.de/user/Podstock/collection/podstock2018/opml" "fyyd-de-podstock-collection-podstock2018.opml"
-#MirrorOPML "https://fyyd.de/user/rebel/collection/997f494a3a31d9885acd820499c0439a/opml" "fyyd-de-rebel-collecion.opml"
-#MirrorOPML "https://fyyd.de/user/Sliebschner/collection/comic-podcasts/opml" "fyyd-de-sliebschner-collection-comic-podcasts.opml"
 
 MirrorOPML "https://lists.pocketcasts.com/20-under-20.opml" "pocketcasts-com-20-under-20.opml"
 MirrorOPML "https://lists.pocketcasts.com/20under20.opml" "pocketcasts-com-20under20.opml"
